bf8606da779c5361694b42486c06029f96a624bf
Nathan Chapman <nathan@nchp.mn>
2017-09-17 11:38:23 +1000

    Home page templating - first three sections

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,5 +9,40 @@
     <h2><?= $page->bannertitle() ?><br />
         <span><?= $page->bannersubtitle() ?></span></h2>
 </section>
+        
+<!-- One -->
+<section id="one" class="wrapper style1">
+    <div class="container 75%">
+        <div class="row 200%">
+            <div class="6u 12u$(medium)">
+                <header class="major">
+                    <h2><?= $page->introtitle() ?></h2>
+                    <p><?= $page->introsubtitle() ?></p>
+                </header>
+            </div>
+            <div class="6u$ 12u$(medium)">
+                <?= $page->introblurb()->kirbytext() ?>
+            </div>
+        </div>
+    </div>
+</section>
+        
+<!-- Two -->
+<section id="two" class="wrapper style2 special">
+    <div class="container">
+        <div class="row 150%">
+            <?php foreach($page->quicklinks()->toStructure() as $link): ?>
+            <div class="3u 6u(small) 12u$(xsmall)">
+                <div class="image fit captioned">
+                   <a href="<?= $site->page($link->pagename())->url() ?>">
+                   <?= $link->thumbnail()->toFile()->crop(600,400) ?>
+                   <h3><?= $site->page($link->pagename())->title() ?></h3>
+                    </a>
+                </div>
+            </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+</section>
 
 <?php snippet('footer') ?>
